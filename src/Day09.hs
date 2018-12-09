4ea705a592802fdb61b4e9a4a86b3404e8264faf
Fabs <fabriciosn@gmail.com>
2018-12-09 22:07:01 +0100

    fix: day9 performance

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,5 +1,7 @@
 module Day09 where
 
+import Data.List.PointedList.Circular as R
+import Data.Vector as V
 import Debug.Trace
 import Fabs.LazyStdin (runOnInput)
 
@@ -11,59 +13,33 @@ star1 p m e = print (e, game p m)
 star2 :: Int -> Int -> IO ()
 star2 _ _ = print =<< runOnInput id
 
-game p m = maximum . V.toList $ gameStep marbles p 0 startScore 1 board head'
+game p m = V.maximum $ gameStep marbles p 0 startScore board
   where
     startScore = V.replicate (p + 1) 0
-    board = V.replicate m 0
+    board = R.singleton 0
     head' = 0
     marbles = [1 .. m]
 
 gameStep ::
-     [Int]
-  -> Int
-  -> Int
-  -> V.Vector Int
-  -> Int
-  -> V.Vector Int
-  -> Int
-  -> V.Vector Int
-gameStep [] _ _ s _ _ _ = s
-gameStep (m:ms) p cp s l b h
-  | m `mod` 23 == 0 = specialPlacement (m : ms) p cp s l b h
-  | otherwise = normalPlacement (m : ms) p cp s l b h
+     [Int] -> Int -> Int -> V.Vector Int -> R.PointedList Int -> V.Vector Int
+gameStep [] _ _ s _ = s
+gameStep (m:ms) p cp s r
+  | m `mod` 23 == 0 = specialPlacement (m : ms) p cp s r
+  | otherwise = normalPlacement (m : ms) p cp s r
 
 specialPlacement ::
-     [Int]
-  -> Int
-  -> Int
-  -> V.Vector Int
-  -> Int
-  -> V.Vector Int
-  -> Int
-  -> V.Vector Int
-specialPlacement (m:ms) p cp s l b h = gameStep ms p np score (l - 1) board lb
+     [Int] -> Int -> Int -> V.Vector Int -> R.PointedList Int -> V.Vector Int
+specialPlacement (m:ms) p cp s r = gameStep ms p np score nr
   where
     addScore = (V.!) s cp + rM + m
     score = (V.//) s [(cp, addScore)]
-    board = V.filter (/= rM) b
-    lb = cycleIdx (h - 7) l
-    r1 = cycleIdx (lb + 1) l
-    rM = (V.!) b lb
-    np = traceShow m $ (cp + 1) `mod` p
+    rM = _focus . R.moveN (-8) $ r
+    nr = R.next . (maybe (R.singleton 0) id) . R.delete . R.moveN (-8) $ r
+    np = (cp + 1) `mod` p
 
 normalPlacement ::
-     [Int]
-  -> Int
-  -> Int
-  -> V.Vector Int
-  -> Int
-  -> V.Vector Int
-  -> Int
-  -> V.Vector Int
-normalPlacement (m:ms) p cp s l b h = gameStep ms p np s (l + 1) board lb
+     [Int] -> Int -> Int -> V.Vector Int -> R.PointedList Int -> V.Vector Int
+normalPlacement (m:ms) p cp s r = gameStep ms p np s nr
   where
-    board = (V.slice 0 lb b) V.++ (V.singleton m) V.++ (V.slice lb (l - lb) b)
-    lb = cycleIdx (h + 2) l
-    np = traceShow $ (cp + 1) `mod` p
-
-cycleIdx i l = (l + i `mod` l) `mod` l
+    nr = R.next . R.insert m $ r
+    np = (cp + 1) `mod` p
