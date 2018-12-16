aa81cc0a516516ae078ab94e18464c43756f85d3
Fabs <fabriciosn@gmail.com>
2018-12-16 19:13:14 +0100

    optimize 14 for bytestrings

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,29 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day14 where
 
-import qualified Data.Vector as V
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
 import Debug.Trace
 
 star1 :: Int -> Int -> IO ()
-star1 p x = print (x, solve p 0 (V.fromList [3, 7]) (0, 1))
+star1 p x = print (x, solve p "910109" "37" (0, 1))
 
-solve :: Int -> Int -> V.Vector Int -> (Int, Int) -> ((Int, Int), V.Vector Int)
+solve ::
+     Int
+  -> B.ByteString
+  -> B.ByteString
+  -> (Int, Int)
+  -> ((Int, Int), B.ByteString, Int)
 solve p s ls e@(e1, e2)
-  | p + 11 < V.length ls = (e, V.take 10 . V.drop p $ ls)
-  | otherwise = solve p s ls ne
+  | B.length lastMatch /= 0 = (e, lastMatch, B.length ls)
+  | p + 11 < B.length ls = (e, B.take 10 . B.drop p $ ls, 0)
+  | otherwise = solve p s nls ne
   where
-    ve1 = (ls V.! e1)
-    ve2 = (ls V.! e2)
+    lastMatch = snd tailMatch
+    tailMatch = B.breakSubstring s . B.drop (B.length ls - 20) $ ls
+    ve1 = fst . fromMaybe (0, "") $ BC.readInt (B.singleton . B.index ls $ e1)
+    ve2 = fst . fromMaybe (0, "") $ BC.readInt (B.singleton . B.index ls $ e2)
     ie1 = 1 + ve1 + e1
     ie2 = 1 + ve2 + e2
-    nnums = nums (ve1 + ve2)
-    new = length ls + length nnums
+    nnums = BC.pack . show $ (ve1 + ve2)
+    new = B.length ls + B.length nnums
     ne = ord' ((ie1 `mod` new), (ie2 `mod` new))
-    -- (p', l', e', s') = shorten s p ((V.++) ls nnums) ne
+    nls = B.concat [ls, nnums]
 
 ord' (a, b)
   | a < b = (a, b)
   | otherwise = (b, a)
-
-nums x
-  | x <= 9 = V.singleton x
-  | otherwise = V.fromList [1, x `mod` 10]
